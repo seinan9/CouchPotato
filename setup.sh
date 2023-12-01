ecfee803d4d7d6e8be2c693527a94f0594e03616
Sinan Kurtyigit <sinan.kurtyigit@gmail.com>
2023-12-01 12:54:39 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #!bin/sh
 
+data_dir=$1
+
 # Create data directory
-mkdir -p .data/datasets
-mkdir -p .data/models
-mkdir -p .data/output
+mkdir -p $1/data/datasets
+mkdir -p $1/data/models
+mkdir -p $1/data/output
 
 # Create and activate virtual environment
 python -m venv ./venv
-source .venv/bin/activate
+source venv/bin/activate
 
 # TODO: Install dependencies
