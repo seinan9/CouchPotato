f1c7ad5efc6ddd69a4f04d5a434c55837eb6d34b
Sinan Kurtyigit <sinan.kurtyigit@gmail.com>
2023-12-01 12:02:00 +0100

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!bin/sh
+
+# Create data directory
+mkdir -p .data/datasets
+mkdir -p .data/models
+mkdir -p .data/output
+
+# Create and activate virtual environment
+python -m venv ./venv
+source .venv/bin/activate
+
+# TODO: Install dependencies
